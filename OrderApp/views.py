c3d8fa3a2d491fa36820845642a91f0909e0ecc0
rimalumesh <umesh.rml@gmail.com>
2026-06-21 05:53:47 +0545

    feat: Placing Order is ready

--- OrderApp/views.py
+++ OrderApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .decoraters import role_required
 from AccountApp.models import User
-from .models import Table
+from .models import Table,Catagory
+
 
 
 # Create your views here.
@@ -11,5 +12,13 @@ def tables_view(request):
     
     return render(request, 'OrderApp/tables.html',{
         'tables':tables
+    })
+
+@role_required([User.ROLE_CHOICES.WAITER])
+def menu_view(request,table_id):
+    categories = Catagory.objects.all()
+    return render(request, 'OrderApp/menu.html',{
+        'categories':categories,
+        'table_id': table_id
     })
 
